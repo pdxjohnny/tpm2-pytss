e7a9f89cc9a0fa66a12d22c0121c5e9e5b674c41
John Andersen <johnandersenpdx@gmail.com>
2020-12-02 12:07:52 -0800

    setup: Specify Python 3 for Cython
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- setup.py
+++ setup.py
@@ -6,4 +6,7 @@ ext_modules = [
     Extension("bill", sources=["bill.pyx", "libbill.c"]),
 ]
 
-setup(name="pyesys", ext_modules=cythonize(ext_modules))
+setup(
+    name="pyesys",
+    ext_modules=cythonize(ext_modules, compiler_directives={"language_level": "3"}),
+)
