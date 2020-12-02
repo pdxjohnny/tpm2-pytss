544d14ad28aaa8187a091bfacd6bcfbeb60ed90f
John Andersen <johnandersenpdx@gmail.com>
2020-12-02 08:52:34 -0800

    style: Format with black
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- setup.py
+++ setup.py
@@ -2,17 +2,8 @@ from setuptools import Extension, setup
 from Cython.Build import cythonize
 
 ext_modules = [
-    Extension("pyesys",
-        sources=["pyesys.pyx"],
-        libraries=["tss2-esys"]
-        ),
-    Extension("bill",
-        sources=["bill.pyx"],
-        libraries=["bill"],
-        library_dirs=["."]
-        )
+    Extension("pyesys", sources=["pyesys.pyx"], libraries=["tss2-esys"]),
+    Extension("bill", sources=["bill.pyx"], libraries=["bill"], library_dirs=["."]),
 ]
 
-setup(name="pyesys",
-    ext_modules = cythonize(ext_modules)
-)
+setup(name="pyesys", ext_modules=cythonize(ext_modules))
