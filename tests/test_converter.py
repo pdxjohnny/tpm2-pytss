bf179a9cc401e90ee76672b21973b2545b9c71cd
John Andersen <johnandersenpdx@gmail.com>
2020-12-15 21:04:44 -0800

    tests: converter: Using Bill sizeof
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -4,21 +4,82 @@ import unittest
 
 from pycparser import c_parser, c_ast, c_generator, parse_file
 
+import pathlib
+import textwrap
+import tempfile
+import subprocess
+from typing import List
+
+
+PROGRAM = r"""
+int main() {
+    printf("%zu\n", sizeof(SIZEOF_TYPE));
+    return 0;
+}
+"""
+
+
+def calculate_sizeof(
+    sizeof_argument,
+    *,
+    system_headers: List[str] = None,
+    relative_headers: List[str] = None,
+    include_dirs: List[pathlib.Path] = None,
+):
+    pound_include_lines = ["#include <stdio.h>"]
+    if system_headers:
+        pound_include_lines += [f"#include <{header}>" for header in system_headers]
+    if relative_headers:
+        pound_include_lines += [f'#include "{header}"' for header in relative_headers]
+
+    with tempfile.TemporaryDirectory(prefix="sizeof_source_") as tempdir:
+        sizeof_path = pathlib.Path(tempdir, "sizeof.c")
+        sizeof_path.write_text(
+            textwrap.dedent("\n".join(pound_include_lines + [PROGRAM]))
+        )
+
+        cmd = [
+            "gcc",
+            "-o",
+            str(pathlib.Path(tempdir, "a.out")),
+            "-D",
+            "SIZEOF_TYPE={}".format(sizeof_argument),
+        ]
+        # Add include dirs to search for headers
+        if include_dirs:
+            cmd.append("-I")
+            for include_dir in include_dirs:
+                cmd.append(str(include_dir))
+
+        # Source file to compile
+        cmd.append(str(sizeof_path))
+        subprocess.check_call(cmd)
+
+        return int(subprocess.check_output(str(pathlib.Path(tempdir, "a.out"))))
+
+
 if sys.version_info.major >= 3 and sys.version_info.minor < 9:
-    pathlib.Path.is_relative_to = lambda self, other: str(self.resolve()).startswith(str(other))
+    pathlib.Path.is_relative_to = lambda self, other: str(self.resolve()).startswith(
+        str(other)
+    )
 
 SOURCE_ROOT_DIR = pathlib.Path("/home/pdxjohnny/Documents/c/tpm2-tss").resolve()
 
 C_GENERATOR = c_generator.CGenerator()
 
+
 class CouldNotDetermineStructMemberTypeError(Exception):
     def __init__(self, decl):
-        super().__init__(f"Could not determine struct member type of {decl.coord}: {decl}")
+        super().__init__(
+            f"Could not determine struct member type of {decl.coord}: {decl}"
+        )
 
 
 class CouldNotDetermineStructMemberArraySizeError(Exception):
     def __init__(self, decl):
-        super().__init__(f"Could not determine struct member array size of {decl.coord}: {decl}")
+        super().__init__(
+            f"Could not determine struct member array size of {decl.coord}: {decl}"
+        )
 
 
 def Typedef_TypeDecl_Struct_Decl_Type(decl):
@@ -42,8 +103,17 @@ def Typedef_TypeDecl_Struct_Decl_Array_Size(decl) -> int:
         if isinstance(decl.dim, c_ast.Constant):
             return int(decl.dim.value)
         else:
-            # TODO Compile this using Bill's stuff
-            return C_GENERATOR.visit(decl.dim)
+            # TODO Configurable headers locations
+            return int(
+                calculate_sizeof(
+                    C_GENERATOR.visit(decl.dim),
+                    system_headers=[
+                        pathlib.Path(decl.coord.file).relative_to(
+                            SOURCE_ROOT_DIR / "include"
+                        )
+                    ],
+                )
+            )
     raise CouldNotDetermineStructMemberArraySizeError(decl)
 
 
@@ -82,21 +152,23 @@ class TypedefVisitor(c_ast.NodeVisitor):
         if not pathlib.Path(node.coord.file).is_relative_to(SOURCE_ROOT_DIR):
             return
 
-
         result = self._visit_Typedef(node)
 
         if result is not None:
             print(result)
 
         return
-        print('%s at %s' % (node.decl.name, node.decl.coord))
+        print("%s at %s" % (node.decl.name, node.decl.coord))
 
 
 def show_func_defs(filepath: pathlib.Path):
     # Note that cpp is used. Provide a path to your own cpp or
     # make sure one exists in PATH.
-    ast = parse_file(str(filepath.resolve()), use_cpp=True,
-                     cpp_args='-I' + str(filepath.resolve().parent.parent))
+    ast = parse_file(
+        str(filepath.resolve()),
+        use_cpp=True,
+        cpp_args="-I" + str(filepath.resolve().parent.parent),
+    )
 
     v = TypedefVisitor()
     v.visit(ast)
--- tests/test_tss2_common.py
+++ tests/test_tss2_common.py
@@ -1,4 +1,5 @@
 import tpm2_pytss.tss2_common
+
 # import tpm2_pytss.tss2_tpm2_types
 
 from pprint import pprint
@@ -7,4 +8,3 @@ pprint(tpm2_pytss.tss2_common.__dict__)
 
 # pprint(tpm2_pytss.tss2_tpm2_types.__dict__)
 # pprint(tpm2_pytss.tss2_tpm2_types.TPM2_MAX_AC_CAPABILITIES)
-
